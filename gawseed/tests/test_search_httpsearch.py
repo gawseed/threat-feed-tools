a95bb4c4e80ac94bbf5b827b859fdce4e6c46654
Wes Hardaker <opensource@hardakers.net>
2020-01-07 15:51:23 -0800

    http bro based search

---
+++ gawseed/tests/test_search_httpsearch.py
@@ -0,0 +1,12 @@
+import unittest
+
+class test_base_search(unittest.TestCase):
+    def test_load_base_search(self):
+        import gawseed.threatfeed.search.http
+        self.assertTrue(True, "imported gawseed.threatfeed.search.http")
+
+        created = gawseed.threatfeed.search.http.HTTPSearch([])
+        self.assertEqual(type(created),
+                         gawseed.threatfeed.search.http.HTTPSearch,
+                         "created a gawseed.threatfeed.search.http.HTTPSearch")
+        
---
+++ gawseed/threatfeed/search/http.py
@@ -0,0 +1,27 @@
+from gawseed.threatfeed.search import Search
+
+class HTTPSearch(Search):
+    def __init__(self, search_list,
+                 data_iterator=None,
+                 binary_search=False):
+        super().__init__(search_list, data_iterator, binary_search)
+
+        self._relist = []
+        for item in search_list:
+            compiled = re.compile(item)
+            self._relist.append({ 'match': search_list[item],
+                                  're': compiled})
+    
+    def search(self, row):
+        if b'host' not in row or b'uri' not in row or row[b'host'] is None or row[b'uri'] is None:
+            return None
+        if self._binary_search:
+            url = "http://" + row[b'host'].decode() + "/" + row[b'uri'].decode()
+        else:
+            url = "http://" + row[b'host'].decode() + "/" + row[b'uri'].decode()
+
+        for expr in self._relist:
+            match = expr['re'].match(url)
+            if match:
+                return expr['match']
+
