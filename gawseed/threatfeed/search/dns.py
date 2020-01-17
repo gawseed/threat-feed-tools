ac22fb84307106aebc5420888469e45fab8e8325
Wes Hardaker <opensource@hardakers.net>
2020-01-16 16:52:09 -0800

    if binary, convert key to binary

--- gawseed/threatfeed/search/dns.py
+++ gawseed/threatfeed/search/dns.py
@@ -7,5 +7,6 @@ class DNSSearch(RESearch):
     def __init__(self, search_list, data_iterator, binary_search, conf={}):
         super().__init__(search_list, data_iterator, binary_search, conf)
         self._key = self.config('key', 'query') # assumes bro
-    
+        if binary_search:
+            self._key = bytes(self._key, 'utf-8')
 
