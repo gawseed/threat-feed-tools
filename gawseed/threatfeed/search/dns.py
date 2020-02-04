0c38b31617150361ba1d948b64e5a4f5d125e505
Wes Hardaker <opensource@hardakers.net>
2020-02-04 07:29:13 -0800

    api parameter fixes

--- gawseed/threatfeed/search/dns.py
+++ gawseed/threatfeed/search/dns.py
@@ -6,7 +6,7 @@ from gawseed.threatfeed.search.re import RESearch
 class DNSSearch(RESearch):
     """Searches for a threat regexp in DNS query logs"""
     def __init__(self, conf, search_list, data_iterator, binary_search):
-        super().__init__(conf, search_list, conf, data_iterator, binary_search)
+        super().__init__(conf, search_list, data_iterator, binary_search)
         self._key = self.config('key', 'query',
                                 help="The key of the field in the data stream to search for matches") # assumes bro
         if binary_search:
