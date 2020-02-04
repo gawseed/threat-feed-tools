12a026c490147f8801598cb62b2566d548b85e35
Wes Hardaker <opensource@hardakers.net>
2020-02-04 14:06:09 -0800

    working version of the datasource enrichment

--- gawseed/threatfeed/enrichments/datasource.py
+++ gawseed/threatfeed/enrichments/datasource.py
@@ -3,8 +3,9 @@ import time
 import sys
 
 from gawseed.threatfeed.config import Config
+import gawseed.threatfeed.loader as loader
 
-class EnrichmentURL(Config):
+class Datasource(Config):
     """Pulls enrichment data from another datasource based on a key
     ("match_key") in the row that was matched by the searcher.  It
     matches this key against a key from the datasource:
@@ -23,22 +24,58 @@ class EnrichmentURL(Config):
         self._match_key = self.config('match_key', 'id_orig_h',
                                       help="The key to use when extracting search data from the matched row.")
 
+        self._match_time_column = self.config('datasource_time_column', 'timestamp',
+                                          help="The time column to use when extracting a match timestamp to search forward and backward from")
+        
         self._datasource_key = self.config('datasource_key', 'id_orig_h',
                                            help="The key to use when searching through the datasource.")
 
-        self._datasource_timekey = self.config('datasource_timekey', 'timestamp',
-                                               help="The time column to use when searching forward and backward")
-
         self._time_backward = self.config('time_backward', '2m', datatype='offset',
                                           help="The amount of time to get data from going backward from the match point")
-        self._time_forard = self.config('time_forward', '2m', datatype='offset',
-                                          help="The amount of time to get data from going forward from the match point")
+        self._time_forward = self.config('time_forward', '2m', datatype='offset',
+                                         help="The amount of time to get data from going forward from the match point")
+
+        self._output_key = self.config('output_key', 'datasource',
+                                       help="The output key to store the returned data in.")
 
     def gather(self, count, row, match):
-        timestamp = float(row[self._datasource_timekey])
 
-        ds_config = self._config
+        ds_config = self._datasource
+
+        timestamp = float(row[self._match_time_column])
         ds_config['begin_time'] = '@' + str(timestamp - self._time_backward)
-        ds_config['end_time'] = '@' + str(timestamp - self._time_forward)
+        ds_config['end_time'] = '@' + str(timestamp + self._time_forward)
 
-        ds = 
+        conf = { loader.YAML_KEY: [{loader.DATASOURCE_KEY: ds_config}] }
+
+        data_source = loader.create_instance(conf, loader.DATASOURCE_KEY)
+        data_source.initialize()
+
+        print("------------------------------***")
+        try:
+            data_source.open()
+        except Exception as e:
+            print("end of file? " + str(e))
+            return (self._output_key, []) # end of file
+        print("------------------------------***")
+
+        # how we should do it eventually:
+        # conf = { loader.YAML_KEY: [{loader.SEARCHER_KEY: { 'module': 'ip',
+        #                                                    'search_keys': [self._datasource_key]}}] }
+        # searcher = loader._create_instance()
+
+        match_value = row[self._match_key]
+        
+        print("------------------------------***")
+        enrich_rows = []
+        print(match_value)
+        print(self._datasource_key)
+        try:
+            for enrich_row in data_source:
+                if enrich_row[self._datasource_key] == match_value:
+                    enrich_rows.append(enrich_row)
+        except Exception as e:
+            print("done? " + str(e))
+
+        return (self._output_key, enrich_rows)
+                
