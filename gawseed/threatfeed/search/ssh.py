6173c39d3b43c79cfcabaef14aab8ff0da79e15e
Wes Hardaker <opensource@hardakers.net>
2020-02-07 15:46:38 -0800

    fix auth_success key/values

--- gawseed/threatfeed/search/ssh.py
+++ gawseed/threatfeed/search/ssh.py
@@ -9,15 +9,16 @@ class SSHSearch(IPSearch):
         super().__init__(conf, search_list, data_iterator, binary_search)
         # Really need a tri-nary option for this
 
-        auth_success_key = self.config('auth_success_key', 'auth_success',
-                                       help="When searching for authenticated ssh connections, use this column name to determine if authentication suceeded")
-        auth_success_value = self.config('auth_success_value', True,
-                                         help="The value that should match the authentication field identified by auth_success_key")
+        self._auth_success_key = self.config('auth_success_key', 'auth_success',
+                                             help="When searching for authenticated ssh connections, use this column name to determine if authentication suceeded")
+        self._auth_success_value = self.config('auth_success_value', True,
+                                               help="The value that should match the authentication field identified by auth_success_key")
 
     def initialize(self):
         super().initialize()
         self._auth_success_key = self._data_iterator.encode_item(self._auth_success_key)
-        self._auth_success_value = self._data_iterator.encode_item(self._auth_success_value)
+        if type(self._auth_success_value) == str:
+            self._auth_success_value = self._data_iterator.encode_item(self._auth_success_value)
     
     def search(self, row):
         if not self._auth_success_key or self._auth_success_key not in row:
