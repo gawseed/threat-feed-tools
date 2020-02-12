c20e62532e41ccb37de6f5cdf5e9546bfb79feb4
Wes Hardaker <opensource@hardakers.net>
2020-02-12 11:47:34 -0800

    search_key bug fix

--- gawseed/threatfeed/search/ip.py
+++ gawseed/threatfeed/search/ip.py
@@ -4,11 +4,11 @@ class IPSearch(Search):
     """Searches for IP address threats in multiple search_keys fields"""
     def __init__(self, conf, search_list, data_iterator, binary_search):
         super().__init__(conf, search_list, data_iterator, binary_search)
-        search_keys = self.config('search_keys', ['id_orig_h', 'id_resp_h'],
-                                  help="A list of fields to search for IP addresses in the data stream")
+        self._search_keys = self.config('search_keys', ['id_orig_h', 'id_resp_h'],
+                                        help="A list of fields to search for IP addresses in the data stream")
 
     def initialize(self):
-        self._search_keys = self._data_iterator.encode_list(search_keys)
+        self._search_keys = self._data_iterator.encode_list(self._search_keys)
     
     def search(self, row):
         for key in self._search_keys:
