0cd0ae29c0e97ff070eaf7fe0ccb0365a651d97d
Wes Hardaker <opensource@hardakers.net>
2020-02-26 07:17:33 -0800

    fix search api change

--- gawseed/tests/test_re_search.py
+++ gawseed/tests/test_re_search.py
@@ -5,7 +5,7 @@ class test_base_search(unittest.TestCase):
         import gawseed.threatfeed.search.re
         self.assertTrue(True, "imported gawseed.threatfeed.search.re")
 
-        created = gawseed.threatfeed.search.re.RESearch([], None, False, {'key': 'key'})
+        created = gawseed.threatfeed.search.re.RESearch({'key': 'key'}, [], None, False)
         self.assertEqual(type(created),
                          gawseed.threatfeed.search.re.RESearch,
                          "created a gawseed.threatfeed.search.re.RESearch")
