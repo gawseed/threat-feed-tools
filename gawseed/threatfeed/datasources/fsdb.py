2c841d42b56658ad747999689e77ba05b7d14ef1
Wes Hardaker <opensource@hardakers.net>
2020-03-28 12:45:34 -0700

    Accept a file_handle of 'stdin'

--- gawseed/threatfeed/datasources/fsdb.py
+++ gawseed/threatfeed/datasources/fsdb.py
@@ -6,7 +6,7 @@ class FsdbDataSource(DataSource):
     def __init__(self, conf):
         super().__init__(conf)
         
-        self._file_handle = self.config('file_handle',
+        self._file_handle = self.config('file_handle', datatype='file_handle',
                                         help="A python3 opened file handle for the BRO data to be streamed")
         self._file = self.config('file',
                                  help="The file name to read the bro data stream from")
