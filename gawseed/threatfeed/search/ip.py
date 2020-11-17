e27af5ce49e7aeeae83097a7d1d6d7472ca64f0f
Wes Hardaker <opensource@hardakers.net>
2020-11-17 01:22:41 -0800

    white space

--- gawseed/threatfeed/search/ip.py
+++ gawseed/threatfeed/search/ip.py
@@ -8,8 +8,8 @@ class IPSearch(Search):
         super().__init__(conf, search_list, data_iterator, binary_search)
         self._search_keys = self.config('search_keys',
                                         ['id_orig_h', 'id_resp_h'],
-                                        help="A list of fields to search for IP" +
-                                        "addresses in the data stream")
+                                        help="A list of fields to search " +
+                                        "for IP addresses in the data stream")
 
     def initialize(self):
         super().initialize()
