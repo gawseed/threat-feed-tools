880d6b289a9941ea349a3822f526610d3f437fa8
Wes Hardaker <opensource@hardakers.net>
2020-11-17 11:06:57 -0800

    test searching of binary data returns binary and ascii both unmodified

--- gawseed/tests/test_search_base.py
+++ gawseed/tests/test_search_base.py
@@ -19,6 +19,25 @@ def fake_search(row):
     return False
 
 
+def fake_true(row):
+    return row
+
+
+class fakebinary():
+    def __init__(self):
+        self.data = ['abcd', b'abcd']
+        self._binary = False
+
+    def __iter__(self):
+        for string in self.data:
+            yield string
+
+    def convert_row_to_utf8(self, row):
+        from gawseed.threatfeed.datasources import DataSource
+        DataSource.convert_row_to_utf8(self, row)
+
+
+
 class test_base_search(unittest.TestCase):
     def test_load_base_search(self):
         import gawseed.threatfeed.search
@@ -36,8 +55,24 @@ class test_base_search(unittest.TestCase):
                                                    fakedata(), False)
         created.search = fake_search
         for match in created:
-            self.assertEqual(match, (10,10), "match was true")
+            self.assertEqual(match, (10, 10), "match was true")
         self.assertTrue(isinstance(created, collections.Iterable), "is type")
+        self.assertTrue(True, "got end of searching")
+
+    def test_binary(self):
+        import collections
+        import gawseed.threatfeed.search
+        created = gawseed.threatfeed.search.Search({}, [4242],
+                                                   fakebinary(), False)
+        created.search = fake_true
+
+        matches = []
+        for match in created:
+            matches.append(match)
+
+        self.assertEqual(matches[0], (None, 'abcd'), "ascii was true")
+        self.assertEqual(matches[1], (None, b'abcd'), "binary was true")
+
         self.assertTrue(True, "got end of searching")
 
     def test_status_queue(self):
