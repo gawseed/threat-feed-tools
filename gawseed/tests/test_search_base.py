8c24532cf94d879a6de68054a2367324b12b0060
Wes Hardaker <opensource@hardakers.net>
2020-11-17 01:13:54 -0800

    fix iteration in search and add a search feedback queue

--- gawseed/tests/test_search_base.py
+++ gawseed/tests/test_search_base.py
@@ -1,5 +1,24 @@
 import unittest
 
+
+class fakedata():
+    def __init__(self):
+        self.data = range(30)
+
+    def __iter__(self):
+        for num in self.data:
+            yield num
+
+    def convert_row_to_utf8(self, row):
+        return row
+
+
+def fake_search(row):
+    if row == 10:
+        return 10
+    return False
+
+
 class test_base_search(unittest.TestCase):
     def test_load_base_search(self):
         import gawseed.threatfeed.search
@@ -9,4 +28,41 @@ class test_base_search(unittest.TestCase):
         self.assertEqual(type(created),
                          gawseed.threatfeed.search.Search,
                          "created a gawseed.threatfeed.search.Search")
-        
+
+    def test_searcher(self):
+        import collections
+        import gawseed.threatfeed.search
+        created = gawseed.threatfeed.search.Search({}, [4242],
+                                                   fakedata(), False)
+        created.search = fake_search
+        for match in created:
+            self.assertEqual(match, (10,10), "match was true")
+        self.assertTrue(isinstance(created, collections.Iterable), "is type")
+        self.assertTrue(True, "got end of searching")
+
+    def test_status_queue(self):
+        import gawseed.threatfeed.search
+        import queue
+
+        status_queue = queue.Queue()
+        created = gawseed.threatfeed.search.Search({}, [4242],
+                                                   fakedata(), False,
+                                                   status_queue,
+                                                   20)
+        created.search = fake_search
+
+        for match in created:
+            self.assertEqual(match, (10,10), "match was true")
+        self.assertTrue(True, "got end of searching")
+
+        result = status_queue.get()
+        self.assertEqual(result,
+                         {'type': 'searcher',
+                          'subtype': 'counter',
+                          'count': 0})
+
+        result = status_queue.get()
+        self.assertEqual(result,
+                         {'type': 'searcher',
+                          'subtype': 'counter',
+                          'count': 20})
--- gawseed/threatfeed/search/__init__.py
+++ gawseed/threatfeed/search/__init__.py
@@ -1,24 +1,27 @@
 from gawseed.threatfeed.config import Config
 
 class Search(Config):
-    def __init__(self, conf, search_list, data_iterator, binary_search):
+    def __init__(self, conf, search_list, data_iterator, binary_search,
+                 status_queue=None, report_every=10000):
         super().__init__(conf)
         self._search_list = search_list
         self._data_iterator = data_iterator
         self._binary_search = binary_search
+        self._status_queue = status_queue
+        self._counter_threshold = report_every
 
     def initialize(self):
         self._search_list = self._data_iterator.encode_dict(self._search_list)
     
     def __iter__(self):
-        return self
-
-    def __next__(self):
-        for row in self._data_iterator:
+        for (n, row) in enumerate(self._data_iterator):
             match = self.search(row)
             if match:
                 row = self._data_iterator.convert_row_to_utf8(row)
                 yield (row, match)
 
-
-        
+            # report our status if asked
+            if self._status_queue and n % self._counter_threshold == 0:
+                self._status_queue.put({'type': 'searcher',
+                                        'subtype': 'counter',
+                                        'count': n})
