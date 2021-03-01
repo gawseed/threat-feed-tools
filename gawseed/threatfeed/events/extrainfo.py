526ed23c6d719132a5caaa5fbddee1da5d25283e
Wes Hardaker <opensource@hardakers.net>
2021-03-01 08:38:00 -0800

    refactor extra information var name

--- gawseed/threatfeed/events/extrainfo.py
+++ gawseed/threatfeed/events/extrainfo.py
@@ -7,29 +7,29 @@ class ExtraInfo(Config):
     def __init__(self, conf):
         super().__init__(conf)
 
-        self._jinja_extra_information = self.config('extra_information', {},
-                                                    help="A YAML file name to be loaded as an extra_information field passed to the jinja2 template")
-        self._jinja_extra_information_root = self.config('extra_information_root', 'feeds',
+        self._extra_information = self.config('extra_information', {},
+                                                    help="A YAML file name to be loaded as an extra_information field passed to the extra2 template")
+        self._extra_information_root = self.config('extra_information_root', 'feeds',
                                                     help="Optional root for the extra information to convert to a dictonary")
-        self._jinja_extra_information_tag = self.config('extra_information_key', 'tag',
+        self._extra_information_tag = self.config('extra_information_key', 'tag',
                                                     help="Optional tag for the extra information to convert to a dictonary")
 
 
-        self._jinja_extra_information_by_tag = {}
+        self._extra_information_by_tag = {}
 
     def load_extra_info(self):
-        if self._jinja_extra_information:
-            fh = open(self._jinja_extra_information, "r")
-            self._jinja_extra_information = yaml.load(fh,
+        if self._extra_information:
+            fh = open(self._extra_information, "r")
+            self._extra_information = yaml.load(fh,
                                                       Loader=yaml.FullLoader)
 
         # refactor it into a tag dictionary version
-        self._jinja_extra_information_by_tag = {}
+        self._extra_information_by_tag = {}
 
-        if self._jinja_extra_information:
-            root_name = self._jinja_extra_information_root
-            tag_name = self._jinja_extra_information_tag
-            root = self._jinja_extra_information[root_name]
+        if self._extra_information:
+            root_name = self._extra_information_root
+            tag_name = self._extra_information_tag
+            root = self._extra_information[root_name]
             for item in root:
                 if tag_name in item:
-                    self._jinja_extra_information_by_tag[item[tag_name]] = item
+                    self._extra_information_by_tag[item[tag_name]] = item
